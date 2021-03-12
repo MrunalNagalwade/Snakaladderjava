5f6b0d50e47f1a1219180f78996e32a0657cb143
MrunalNagalwade <mrunalnagalwade187@gmail.com>
2021-03-12 07:41:34 +0530

    [mrunal]: Player checks for option

--- Snakeladder.java
+++ Snakeladder.java
@@ -1,7 +1,26 @@
 public class Snakeladder
 {
+	public static final int NO_PLAY = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
 	public static void main(String[] args) {
 	int rolldice = 1 + (int)( (Math.random() *10) % 6);
-	System.out.println("Player rolled dice: " +rolldice);
+	System.out.println("Player rolled dice: " + rolldice);
+	int optionplay = (int) ( (Math.random() * 10) % 3);
+	if(optionplay == 0)
+	{
+		System.out.println("No play");
+ 	}
+
+	else if(optionplay == 1)
+    {
+                System.out.println("ladder");
+    }
+
+	else if(optionplay == 2)
+    {
+                System.out.println("snake");
+    }
 }
 }
+
